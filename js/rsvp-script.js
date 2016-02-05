ec7a11b1d0b2902e0335eb2a3e8baf45e517508e
Felix Abele <felix.abele@infopark.de>
2016-02-05 16:19:50 +0100

    solved ue probs

--- js/rsvp-script.js
+++ js/rsvp-script.js
@@ -55,7 +55,7 @@ var Rsvp = new function() {
     $('#rsvp_form').submit(function(e) {
       e.preventDefault();
       $.each($(this).serializeArray(), function(i, field) {
-        form[field.name] = field.value.replace(/ö|Ö|Ü|ü|ä|Ä/g, "-");
+        form[field.name] = encodeURIComponent(field.value);
       })
 
       if (validate(form)) {
